28277c7fda61704cc3a10b5db134bb93866b5000
Sahil Grover <gr.sahil20@gmail.com>
2021-02-20 19:19:47 +0100

    first commit
    squash me

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    full_name { Faker::Name.name }
+    username  { Faker::Internet.username(separators: %w()) }
+    email     { Faker::Internet.email }
+    password  { "password" }
+
+  end
+end
